0468fd94ef9e5712d200ad934a9db9eb97a03505
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2024-01-27 19:46:24 +0700

    feat: create delete note features

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,25 +1,55 @@
 import React from 'react'
-import { getNote } from '../utils/local-data';
-import { useParams } from 'react-router-dom';
+import { getNote, deleteNote } from '../utils/local-data';
+import { useNavigate, useParams } from 'react-router-dom';
 import { FiTrash } from 'react-icons/fi';
 import { showFormattedDate } from '../utils/index';
 
-function DetailPage() {
-  const  { id } = useParams();
+function DetailPageWrapper() {
+  const { id } = useParams();
+  const navigate = useNavigate();
   const note = getNote(id);
 
-  return(
-    <section>
-      <div className='detail-page'>
-        <h3 className='detail-page__title'>{ note.title }</h3>
-        <p className='detail-page__createdAt'>{showFormattedDate(note.createdAt)}</p>
-        <div className='detail-page__body'>{ note.body }</div>
-        <div className='detail-page__action'>
-          <button className='action' type='button' title='Delete'><FiTrash /></button>
-        </div>
-      </div>
-    </section>
+  return (
+    <DetailPage note={note} navigate={navigate} />
   )
 }
 
-export default DetailPage;
+class DetailPage extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      id: props.note.id,
+      title: props.note.title,
+      body: props.note.body,
+      createdAt: props.note.createdAt,
+      navigate: props.navigate,
+    }
+
+    this.onDeleteHandler = this.onDeleteHandler.bind(this);
+  }
+
+  onDeleteHandler(id) {
+    deleteNote(id);
+
+    this.props.navigate('/');
+  }
+
+
+  render() {
+    return(
+      <section>
+        <div className='detail-page'>
+          <h3 className='detail-page__title'>{ this.state.title }</h3>
+          <p className='detail-page__createdAt'>{ showFormattedDate(this.state.createdAt)}</p>
+          <div className='detail-page__body'>{ this.state.body }</div>
+          <div className='detail-page__action'>
+            <button className='action' type='button' title='Delete' onClick={() => this.onDeleteHandler(this.state.id)}><FiTrash /></button>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default DetailPageWrapper;
