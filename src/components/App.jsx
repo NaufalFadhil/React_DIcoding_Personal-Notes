f6faa07a6864711ddd436791d507e85878ec4175
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2024-01-29 12:17:01 +0700

    feat: create function of theme

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,10 +8,12 @@ import NotFoundPage from '../pages/NotFoundPage';
 import LoginPage from '../pages/LoginPage';
 import { getUserLogged, putAccessToken } from '../utils/network-data';
 import RegisterPage from '../pages/RegisterPage';
+import ThemeContext from '../context/ThemeContext';
 
 function App() {
   const [authedUser, setAuthedUser] = React.useState(null);
   const [initializing, setInitializing] = React.useState(true);
+  const [theme, setTheme] = React.useState('dark');
 
   React.useEffect(() => {
     getUserLogged().then(({ data }) => {
@@ -27,29 +29,44 @@ function App() {
     setAuthedUser(data);
   }
 
+  const toggleTheme = () => {
+    setTheme((prevTheme) => {
+      return prevTheme === 'light' ? 'dark' : 'light';
+    });
+  };
+
+  const contextValue = React.useMemo(() => {
+    return {
+      theme,
+      toggleTheme,
+    };
+  }, [theme]);
+
   if (initializing) {
     return null;
   }
 
   return (
-    <div className="contact-app">
-      <Header />
-      <main>
-        {(authedUser === null) ? (
-          <Routes>
-            <Route path="/*" element={<LoginPage loginSuccess={onLoginSuccess} />} />
-            <Route path="/register" element={<RegisterPage />} />
-          </Routes>
-        ) : (
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/notes/new" element={<AddPage />} />
-            <Route path="/notes/:id" element={<DetailPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        )}
-      </main>
-    </div>
+    <ThemeContext.Provider value={contextValue}>
+      <div className="app-container" data-theme={theme}>
+        <Header />
+        <main>
+          {(authedUser === null) ? (
+            <Routes>
+              <Route path="/*" element={<LoginPage loginSuccess={onLoginSuccess} />} />
+              <Route path="/register" element={<RegisterPage />} />
+            </Routes>
+          ) : (
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/notes/new" element={<AddPage />} />
+              <Route path="/notes/:id" element={<DetailPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          )}
+        </main>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { FiMoon, FiSun } from 'react-icons/fi';
 import { Link } from 'react-router-dom'
+import ThemeContext from '../context/ThemeContext';
 
 function Header () {
+  const { theme, toggleTheme } = React.useContext(ThemeContext);
   return (
     <header>
       <h1><Link to='/'>Personal Notes</Link></h1>
       <div className='navigation'>
         <ul>
-        <li><Link to="/archives">Arsip</Link></li>
+          {/* <li><Link to="/archives">Arsip</Link></li> */}
+          <li><div onClick={toggleTheme}>{theme === 'light' ? <div className='toggle-theme'><FiMoon /></div> : <div className='toggle-theme'><FiSun /></div>}</div></li>
         </ul>
       </div>
     </header>
