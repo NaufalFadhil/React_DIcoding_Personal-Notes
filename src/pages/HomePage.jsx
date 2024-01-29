34d114a525e171545797add2dbbe5e6522df397c
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2024-01-29 16:56:32 +0700

    feat: implement api at home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,88 +1,67 @@
 import React from 'react';
 import NoteList from '../components/NoteList';
-import { deleteNote, getAllNotes } from '../utils/local-data';
+import { deleteNote, getActiveNotes } from '../utils/network-data';
 import SearchBar from '../components/SearchBar';
 import { useSearchParams, Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
 import NoteListEmpty from '../components/NoteListEmpty';
 import PropTypes from 'prop-types';
 
-function HomePageWrapper() {
+function HomePage() {
   const [searchParams, setSearchParams] =  useSearchParams();
-  
-  const keyword = searchParams.get('keyword');
+  const [notes, setNotes] = React.useState([]);
+  const [keyword, setKeyword] = React.useState(() => {
+    return searchParams.get('keyword') || '';
+  });
+
+  React.useEffect(() => {
+    getActiveNotes().then(({ data}) => {
+      setNotes(data);
+    })
+  }, [])
 
   function changeSearchParams(keyword) {
     setSearchParams({ keyword });
   }
 
-  return (
-    <HomePage defaultKeyword={keyword} keywordChange={changeSearchParams} />
-  )
+  async function onDeleteHandler(id) {
+    await deleteNote(id);
 
-
-}
-class HomePage extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      notes: getAllNotes(),
-      keyword: props.defaultKeyword || '',
-    }
-
-    this.onDeleteHandler = this.onDeleteHandler.bind(this);
-    this.onKeywordChangeHandler = this.onKeywordChangeHandler.bind(this);
+    const { data } = await getActiveNotes();
+    setNotes(data);
   }
 
-  onDeleteHandler(id) {
-    deleteContact(id);
-
-    this.setState(() => {
-      return {
-        notes: getAllNotes(),
-      }
-    });
+  function onKeywordChangeHandler(keyword) {
+    setKeyword(keyword);
+    setSearchParams({ keyword });
+    // this.props.keywordChange(keyword);
   }
 
-  onKeywordChangeHandler(keyword) {
-    this.setState(() => {
-      return {
-        keyword,
-      }
-    });
-
-    this.props.keywordChange(keyword);
-  }
-
-  render() {
-    const notes = this.state.notes.filter((contact) => {
-      return contact.title.toLowerCase().includes(
-        this.state.keyword.toLowerCase()
-      )
-    });
-
-    return (
-      <section>
-        <h2>Catatan Aktif</h2>
-        <SearchBar keyword={this.state.keyword} keywordChange={this.onKeywordChangeHandler} />
-        {( notes == 0 
-          ? <NoteListEmpty />
-          : <NoteList notes={notes} onDelete={this.onDeleteHandler} />
-        )}
-        <div className='add-new-page'>
-          <Link to='/notes/new'>
-            <button className='action add-new-page__action'><FiPlus /></button>
-          </Link>
-        </div>
-      </section>
+  const filteredNotes = notes.filter((note) => {
+    return note.title.toLowerCase().includes(
+      keyword.toLowerCase()
     )
-  }
+  });
+
+  return (
+    <section>
+      <h2>Catatan Aktif</h2>
+      <SearchBar keyword={keyword} keywordChange={onKeywordChangeHandler} />
+      {( notes == 0 
+        ? <NoteListEmpty />
+        : <NoteList notes={filteredNotes} onDelete={onDeleteHandler} />
+      )}
+      <div className='add-new-page'>
+        <Link to='/notes/new'>
+          <button className='action add-new-page__action'><FiPlus /></button>
+        </Link>
+      </div>
+    </section>
+  )
 }
 
 HomePage.propTypes = {
-  defaultKeyword: PropTypes.string,
-  keywordChange: PropTypes.func.isRequired,
-}
+  onKeywordChangeHandler: PropTypes.func.isRequired,
+};
 
-export default HomePageWrapper;
+export default HomePage;
